095689280927f5c7f611c7b108aac28f78828ee6
Eunsong Park <run.eunsong@gmail.com>
2022-10-31 21:45:03 +0900

    초기환경 세팅완료

---
+++ app.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const morgan =require('morgan');
+const appDataSource = require('./models/dataSource');
+const app = express();
+
+app.use(cors());
+app.use(morgan('combined'));
+app.use(express.json());
+
+app.get('/ping', (req, res) => {
+  res.json({ message: 'pong' });
+});
+
+const PORT = process.env.PORT;
+
+const start = async () => {
+  await appDataSource.initialize()
+    .then(() => {
+      console.log('Data Source has been initialized');
+    })
+    .catch(() => {
+      console.log('Errors occurred in Data Source initializing');
+      appDataSource.destroy();
+    })
+  try {
+    app.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
+  } catch (err) {
+    console.error(err);
+  }
+}
+start();
