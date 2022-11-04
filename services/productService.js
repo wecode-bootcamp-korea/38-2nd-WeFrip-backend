bafce4e3a6eeaf4bf982f207e140c2ab977b6dc4
csongin <cheonsong37@gmail.com>
2022-11-04 22:34:30 +0900

    [ADD] listpage, filtering 기능 구현

--- services/productService.js
+++ services/productService.js
@@ -2,8 +2,31 @@ const productDao = require('../models/productDao');
 
 const getProducts = async () => {
   return await productDao.getProducts();
-};
+}
+
+const getProductMainCategories = async (mainCategoryName, sort, firstDate, lastDate) => {  
+  if (sort) {
+    return await productDao.mainCategoryFiltering(mainCategoryName, sort, firstDate, lastDate);
+  } else if(firstDate || lastDate) {
+    return await productDao.mainCategoryFiltering(mainCategoryName, sort, firstDate, lastDate);
+  } else {
+    return await productDao.getProductMainCategories(mainCategoryName);
+  }
+}
+
+
+const getProductSubCategories = async (mainCategoryName, subCategoryName, sort, firstDate, lastDate) => {
+  if (sort) {
+    return await productDao.subCategoryFiltering(mainCategoryName, subCategoryName, sort, firstDate, lastDate);
+  } else if (firstDate || lastDate) {
+    return await productDao.subCategoryFiltering(mainCategoryName, subCategoryName, sort, firstDate, lastDate);
+  } else {
+    return await productDao.getProductSubCategories(mainCategoryName, subCategoryName);
+  }
+}
 
 module.exports = {
-  getProducts
+  getProducts,
+  getProductMainCategories,
+  getProductSubCategories
 }
